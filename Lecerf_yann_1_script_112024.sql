0c8034430ddffc52510cdbf4b219228b232f30f2
Belin27000 <y.lecerf@protonmail.com>
2024-12-03 16:38:41 +0100

    [FIX] SQL Script as per metoring session on amount of ligne to have

--- Lecerf_yann_1_script_112024.sql
+++ Lecerf_yann_1_script_112024.sql
@@ -61,7 +61,7 @@
 -- HAVING COUNT(order_reviews.review_score) > 30
 -- ORDER BY avg_review_score ASC
 -- LIMIT 5;
----ORDERS DELIVERY MORE THAN 3 DAYS DELAY---
+---1 ORDERS DELIVERY MORE THAN 3 DAYS DELAY---
 WITH RecentOrders AS (
     SELECT *
     FROM orders
@@ -79,58 +79,60 @@ DelayedOrders AS (
     WHERE order_status <> 'canceled'
         AND JULIANDAY(order_delivered_customer_date) - JULIANDAY(order_estimated_delivery_date) > 3
 )
-SELECT *
-FROM DelayedOrders;
----Sellers with more than 100K real on delivery orders---
-WITH SellerOrders AS (
-    SELECT sellers.seller_id,
-        sellers.seller_zip_code_prefix,
-        sellers.seller_city,
-        sellers.seller_state,
-        SUM(order_pymts.payment_value) AS total_revenue
-    FROM order_items
-        INNER JOIN orders ON order_items.order_id = orders.order_id
-        INNER JOIN sellers ON order_items.seller_id = sellers.seller_id
-        INNER JOIN order_pymts ON orders.order_id = order_pymts.order_id
-    WHERE orders.order_status <> 'canceled'
-    GROUP BY sellers.seller_id
-),
-HighRevenueSellers AS (
-    SELECT *
-    FROM SellerOrders
-    WHERE total_revenue > 100000
-)
-SELECT *
-FROM HighRevenueSellers
-ORDER BY total_revenue DESC;
----Young seller (less than 3 month) with more than 30 products sales ---
-WITH RecentOrders AS (
-    SELECT *
+SELECT DISTINCT order_id
+FROM orders;
+ORDER BY payment_value DESC;
+---2 Sellers with more than 100K real on delivery orders---
+WITH DeliveredOrders AS (
+    SELECT orders.order_id,
+        orders.order_status
     FROM orders
-    WHERE order_purchase_timestamp >= DATE(
-            (
-                SELECT MAX(order_purchase_timestamp)
-                FROM orders
-            ),
-            '-3 month'
-        )
+    WHERE orders.order_status = 'delivered'
 ),
-SellerSales AS (
-    SELECT oi.seller_id,
-        COUNT(oi.order_item_id) AS total_sales
-    FROM RecentOrders o
-        INNER JOIN order_items oi ON o.order_id = oi.order_id
-    GROUP BY oi.seller_id
+SellerRevenue AS (
+    SELECT order_items.seller_id,
+        SUM(order_items.price) AS total_revenue,
+        COUNT(order_items.order_item_id) AS total_item_sold
+    FROM order_items
+        INNER JOIN DeliveredOrders ON order_items.order_id = DeliveredOrders.order_id
+    GROUP BY order_items.seller_id
+)
+SELECT seller_id,
+    total_revenue,
+    total_item_sold
+FROM SellerRevenue
+WHERE total_revenue > 100000
+ORDER BY total_revenue DESC;
+---3 Young seller (less than 3 month) with more than 30 products sales ---
+WITH SellersWOrders AS(
+    SELECT order_items.seller_id,
+        orders.order_purchase_timestamp
+    FROM orders
+        INNER JOIN order_items ON orders.order_id = order_items.order_id
 ),
-ActiveSellers AS (
-    SELECT *
-    FROM SellerSales
-    WHERE total_sales > 30
+MaxPurchaseDate AS(
+    SELECT MAX (order_purchase_timestamp) AS max_purchase_date
+    FROM orders
+),
+NewSellers AS (
+    SELECT seller_id
+    FROM SellersWOrders,
+        MaxPurchaseDate
+    GROUP BY seller_id
+    HAVING MIN(order_purchase_timestamp) >= DATE(max_purchase_date, '-3 month')
+),
+EngagedSellers AS (
+    SELECT order_items.seller_id,
+        SUM(order_items.price) AS total_amount_sold,
+        COUNT(order_items.order_item_id) AS total_items_sold
+    FROM order_items
+        INNER JOIN NewSellers ON NewSellers.seller_id = order_items.seller_id
+    GROUP BY order_items.seller_id
+    HAVING total_items_sold > 30
 )
 SELECT *
-FROM ActiveSellers
-ORDER BY total_sales DESC;
---- 5 zip code with more than 30 reviews with the worth score on the last 12 month---
+FROM EngagedSellers;
+--- 4 zip code with more than 30 reviews with the worth score on the last 12 month---
 WITH RecentOrders AS (
     SELECT *
     FROM orders
